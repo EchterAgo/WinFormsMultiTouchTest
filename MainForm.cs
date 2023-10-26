3f8e881d420a91e7f5b51962886b1516642591ff
Axel Gembe <axel@gembe.net>
2023-10-26 17:05:56 +0700

    initial

---
+++ MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WinFormsMultiTouchTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            NativeMethods.RegisterTouchWindow(Handle, 0);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_TOUCH = 0x240;
+            if (m.Msg == WM_TOUCH)
+            {
+                HandleTouchMessage(m);
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+        private void HandleTouchMessage(Message m)
+        {
+            var inputCount = (int)(m.WParam & 0xFFFF);
+            var inputs = new TOUCHINPUT[inputCount];
+
+            if (NativeMethods.GetTouchInputInfo(m.LParam, inputCount, inputs, Marshal.SizeOf<TOUCHINPUT>()))
+            {
+                try
+                {
+                    for (int i = 0; i < inputCount; i++)
+                    {
+                        int id = inputs[i].dwID;
+                        int x = inputs[i].x;
+                        int y = inputs[i].y;
+                        int mask = inputs[i].dwMask;
+
+                        // Process the touch input for the given touch point (id)
+                        // Use the x, y, and mask values as needed.
+
+                        Debug.WriteLine($"id = {id}, x = {x/100.0f}, y = {y/100.0f}, mask = {mask}");
+                    }
+                }
+                finally
+                {
+                    NativeMethods.CloseTouchInputHandle(m.LParam);
+                }
+            }
+        }
+    }
+}
---
+++ NativeMethods.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WinFormsMultiTouchTest
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterTouchWindow(IntPtr hWnd, uint flags);
+
+        [DllImport("user32.dll")]
+        internal static extern bool GetTouchInputInfo(IntPtr hTouchInput, int cInputs, [In, Out] TOUCHINPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        internal static extern void CloseTouchInputHandle(IntPtr lParam);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct TOUCHINPUT
+    {
+        public int x;
+        public int y;
+        public IntPtr hSource;
+        public int dwID;
+        public int dwFlags;
+        public int dwMask;
+        public int dwTime;
+        public IntPtr dwExtraInfo;
+        public int cxContact;
+        public int cyContact;
+    }
+}
